30cacfaa372533e7410cbd9937e09faf33d923d7
dodont <794779608@qq.com>
2018-12-07 10:35:10 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/android/coolweather/com/coolweather/db/Province.java
@@ -0,0 +1,43 @@
+package android.coolweather.com.coolweather.db;
+
+import android.provider.ContactsContract;
+
+import org.litepal.crud.DataSupport;
+
+import java.security.PublicKey;
+
+/**
+ * Created by Administrator on 2018/12/4.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+}
